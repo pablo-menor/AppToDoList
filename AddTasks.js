d9afa9d2f31cde343c6bb4c5e1f9e1d511478f2e
pablo-menor <pablomenor2001@gmail.com>
2021-03-31 17:28:29 +0200

    Cleaned code

--- AddTasks.js
+++ AddTasks.js
@@ -1,18 +1,7 @@
 'use strict'
-//añadir el index al localSrorage para que al recargar s emantenga el ultimo index
 
 var index = 1;
-/*for(let i = 0; i<localStorage.length;i++){
-    
-    if(localStorage.key(i)=!null){    //Recorrer el LS para ver si ya hay index
-        index = 0; 
-        localStorage.setItem("index",index);
-    }
-    else{
-        index = localStorage.getItem("index");
-    }
 
-}*/
 
 if (localStorage.length < 1) {
     index = 0;
@@ -54,27 +43,6 @@ window.addEventListener('load', function () {
 
     //PROBAR A RECARGAR PAGE UNA SOLA VEZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
-
-    /* document.querySelector(".checkBox").addEventListener('onclick', function(){ //':checked'
-         //removeTask(this);    //INSTEAD OF THIS I NEED TO INTRODUCE THE TASKKKK4       2. mirar sus id, comprobar el p con mismo y elimnarlo, both.
- 
-         let element_checkbox = document.querySelector("#index");
-       
-         console.log(id_checkbox);
- 
-         listOfChecks.forEach(element => {
- 
-             var id_checkbox = element.getAttribute("id");
-             for(var j in listOfParagraphs){
-                 if (j.getAttribute("id")=id_checkbox && element.checked==true) {
-                     console.log("DONE");
-                 }   
-             }
-         });
-         console.log("joder");
-     })
- */
-
 })
 
 function funPrueba() {   //para eliminar
@@ -82,22 +50,18 @@ function funPrueba() {   //para eliminar
 
         var id_checkbox = elementCheck.getAttribute("id");  ///console
 
-
-        // var element2 = element;
         listOfParagraphs.forEach(element => {
 
             if (element.getAttribute("id") === id_checkbox && elementCheck.checked == true) {
                 id_checkbox--;
                 console.log(element.getAttribute("id"));
-                var keyName2 =/* "Task "  + *//*element.getAttribute("id")*/id_checkbox;
+                var keyName2 = id_checkbox;
                 console.log(keyName2);
                 localStorage.removeItem(keyName2);
                 element.remove();
                 elementCheck.remove();
                 listOfParagraphs.pop(element);
                 listOfChecks.pop(elementCheck);
-                // index--;
-                //  x``zx`x`localStorage.setItem("index", index);
 
             }
         })
@@ -154,8 +118,6 @@ function addFromLocalStorage() {
         if (key != "index") {
             var value = localStorage[key];
             listFromLocalS.push(parseInt(key));  //arrayc con los keys
-
-            //addTask(value,index);
             index++;
         }
     }
@@ -164,7 +126,6 @@ function addFromLocalStorage() {
     listFromLocalS.sort(function (a, b) { return a - b });
     console.log(listFromLocalS);
 
-    // for(var elemento in listFromLocalS){
     listFromLocalS.forEach(element => {
         let key_name = element.toString();
         var valuee = localStorage[key_name];
@@ -172,25 +133,5 @@ function addFromLocalStorage() {
         console.log(valuee);
         addTask(valuee, key_name);
     })
-    /* let key_name = elemento.toString();
-     var valuee = localStorage[key_name];
-     console.log("VALOR: ");     
-     console.log(valuee);
-     addTask(valuee,key_name);*/
-    //HACER ALGO CON EL UNDEFINEDDDDDDDDDDDDDDDDDDDD**************************
-    //}
-    // console.log(listFromLocalS);    
-    /* var num_veces = 0;
- 
-     while(num_veces < localStorage.length){
-         var key = localStorage.key(i);
-     }*/
+
 }
-
-;
-
-//AL HACER ADD FROM LOCAL STORAGE COGERLOS ORDENADOS PUESTO QUE SE ENCUENTRAS DESORDENADOS
-//ES NECESARIO QUITAR "TASK" Y DEJAR SOLO NUMEROS PARA PODER COGERLOS ORDENADOS...
-
-
-//1.coger todo el local storage y ordenarlos por keyname, una vez hecho eso, ir cogíendolos y añadirlos .
